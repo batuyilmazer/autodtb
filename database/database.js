b90a9bb1017e4304205e91e034a876a49f2045ae
batuhanyilmazer <batuhanayilmazer@gmail.com>
2023-04-13 21:08:39 +0300

    increased search functionality @database

--- database/database.js
+++ database/database.js
@@ -1,21 +1,106 @@
-var brands = ["BMW","Mercedes","Audi","Volkswagen","Porsche","Ford","Chevrolet","Dodge"];
+const brands = [
+    "Abarth",
+    "Alfa Romeo",
+    "Aston Martin",
+    "Audi",
+    "Bentley",
+    "BMW",
+    "Bugatti",
+    "Cadillac",
+    "Chevrolet",
+    "Chrysler",
+    "Citroën",
+    "Dacia",
+    "Daewoo",
+    "Daihatsu",
+    "Dodge",
+    "Donkervoort",
+    "DS",
+    "Ferrari",
+    "Fiat",
+    "Fisker",
+    "Ford",
+    "Honda",
+    "Hummer",
+    "Hyundai",
+    "Infiniti",
+    "Iveco",
+    "Jaguar",
+    "Jeep",
+    "Kia",
+    "KTM",
+    "Lada",
+    "Lamborghini",
+    "Lancia",
+    "Land Rover",
+    "Landwind",
+    "Lexus",
+    "Lotus",
+    "Maserati",
+    "Maybach",
+    "Mazda",
+    "McLaren",
+    "Mercedes-Benz",
+    "MG",
+    "Mini",
+    "Mitsubishi",
+    "Morgan",
+    "Nissan",
+    "Opel",
+    "Peugeot",
+    "Porsche",
+    "Renault",
+    "Rolls-Royce",
+    "Rover",
+    "Saab",
+    "Seat",
+    "Skoda",
+    "Smart",
+    "SsangYong",
+    "Subaru",
+    "Suzuki",
+    "Tesla",
+    "Toyota",
+    "Volkswagen",
+    "Volvo"
+]
 listBrandItem(brands);
 
+
+var hasCreatedNotFoundLi;
 //Search Button onclick
 function brandSearchFunc(){
+    var hasFoundMatches=false;
     var ul = document.getElementById("brandList");
+
+    if(hasCreatedNotFoundLi==undefined){
+        let notFoundLi = document.createElement("li");
+        notFoundLi.appendChild(document.createTextNode("Not Found"));
+        notFoundLi.setAttribute("id","notFoundLi");
+        ul.appendChild(notFoundLi);
+        hasCreatedNotFoundLi=true;
+    }
+
     let brandSearchInput = document.getElementById("brandSearchInput").value;
     input = brandSearchInput.toLowerCase();
     let brandListItem = document.getElementsByClassName('brandListItem');
+
     for(i = 0; i < brandListItem.length ; i++){
         if(!brandListItem[i].innerHTML.toLowerCase().includes(input)){
             brandListItem[i].style.display="none";
         }
         else {
             brandListItem[i].style.display="list-item";
+            hasFoundMatches=true;
         }
     }
+    if(!hasFoundMatches){
+        notFoundLi.style.display="list-item";
+    } else {
+        notFoundLi.style.display="none";
+    }
 }
+
 
 // Create brandListItem
 function listBrandItem(brand="unnamed brand"){
@@ -24,6 +109,7 @@ function listBrandItem(brand="unnamed brand"){
         for (let i=0; i < brand.length; i++){
             let li = document.createElement("li");
             li.appendChild(document.createTextNode(brand[i]));
+            li.setAttribute("onclick","onBrandListItemClick()");
             ul.appendChild(li);
             li.setAttribute("class", "brandListItem");
         }
@@ -31,7 +117,15 @@ function listBrandItem(brand="unnamed brand"){
     else {
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(brand));
+        li.setAttribute("onclick","onBrandListItemClick()");
         ul.appendChild(li);
         li.setAttribute("class", "brandListItem");
     }
+}
+
+function onBrandListItemClick(){
+    var rightpane = document.getElementById("rightpane");
+    let foo = document.createElement("p");
+    foo.appendChild(document.createTextNode("Clicked"));
+    rightpane.appendChild(foo);
 }
